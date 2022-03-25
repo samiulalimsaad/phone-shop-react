6d0ed67cc55903930bfc161bb4f0354c879670e2
samiulalimsaad <saadnbiuraj@gmail.com>
2022-03-26 00:19:44 +0600

    added website skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import Product from "./components/products/product";
+import SelectedItem from "./components/selectedItem";
+
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    useEffect(() => {
+        fetch(`https://openapi.programming-hero.com/api/phones?search=samsung`)
+            .then((res) => res.json())
+            .then((data) => console.log(data?.data))
+            .catch((e) => console.log(e.message));
+    }, []);
+
+    return (
+        <div>
+            <h1 className="py-4 text-4xl font-semibold text-center text-white bg-blue-500">
+                Phone Shop
+            </h1>
+            <div className="grid grid-cols-3 sm:grid-cols-4">
+                <div className="col-span-2 sm:col-span-3">
+                    <Product />
+                </div>
+                <div className="col-span-1">
+                    <SelectedItem />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/selectedItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectedItem = () => {
+    return <div>Selected Item Page</div>;
+};
+
+export default SelectedItem;
